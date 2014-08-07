754bd8e80f13f35cd53fad74470540150742b052
JD Guzman <jdguzman@mac.com>
2014-08-07 17:42:24 -0500

    fixing some styles and adding about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,5 @@
 class WelcomeController < ApplicationController
   def index; end
+
+  def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get 'about', controller: 'welcome'
+
   root 'welcome#index'
 end
