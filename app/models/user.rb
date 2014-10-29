64c67d76d5c8a21a271710ab9dd8467ee34abe88
Nic Boie <nic@webvolta.com>
2014-10-29 17:27:21 -0500

    Adding fields to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,21 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
-         :validatable
+  include Mongoid::Paperclip
+  include UserDevise # See this concern file for devise related fields
+  has_mongoid_attached_file :profile_image
 
-  ## Database authenticatable
-  field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
+  %i( first_name
+      last_name
+      biography
+      address_line_1
+      address_line_2
+      postal_code
+      state_province
+      country
+      primary_phone ).each do |string_field|
+    field string_field
+  end
 
-  ## Recoverable
-  field :reset_password_token,   type: String
-  field :reset_password_sent_at, type: Time
+  validates :first_name, :last_name, :email, presence: true
 
-  ## Rememberable
-  field :remember_created_at, type: Time
-
-  ## Trackable
-  field :sign_in_count,      type: Integer, default: 0
-  field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
-  field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
-
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
 end
