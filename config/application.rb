5aff0fa46928846728f077a3a052e23040cc746c
Richard Hancock <rjhancock@me.com>
2014-11-08 11:07:54 -0600

    Removed from application global
    Signed-off-by: Richard Hancock <rjhancock@me.com>

--- config/application.rb
+++ config/application.rb
@@ -26,13 +26,5 @@ module Dfc
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
   end
 end
