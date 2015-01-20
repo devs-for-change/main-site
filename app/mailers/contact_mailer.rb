6d472720b126a4aa6f9ebf537bdf0ad37594e280
Nic Boie <boie0025@gmail.com>
2015-01-19 23:04:30 -0600

    Whoops, probably don't want to email the form-filler-outer of their entry..

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_form_entry(contact)
     @contact = contact
-    mail(to: contact.email, subject: 'A new contact form entry has arrived')
+    mail(to: 'everyone@devsforchange.org', subject: 'A new contact form entry has arrived')
   end
 
 end
