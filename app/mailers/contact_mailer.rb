93a45c00028fd57206199e76f1279a30cd5f8851
Nic Boie <boie0025@gmail.com>
2015-01-25 13:00:09 -0600

    Change address to send forms to info@dfc

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_form_entry(contact)
     @contact = contact
-    mail(to: 'everyone@devsforchange.org', subject: 'A new contact form entry has arrived')
+    mail(to: 'info@devsforchange.org', subject: 'A new contact form entry has arrived')
   end
 
 end
