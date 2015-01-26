a8bc779a91f6efcffcdf6273403173d1ee7dc009
Cindy Schaller <cschaller@gmail.com>
2015-01-25 19:35:51 -0600

    #158 Add contact confirmation page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,11 +4,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:success] = "You're inquiry has been successfully submitted."
       # Probably should take this off the main wire, but we have no
       #  worker concept implemented yet.  N.B.
       ContactMailer.contact_form_entry(@contact).deliver
-      redirect_to :root
+      render 'pages/contact_thankyou'
     else
       render 'pages/contact'
     end
