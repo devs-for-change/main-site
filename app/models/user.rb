8eca3470efbe314f09690ea1abd6995e1da12bb8
Nic Boie <boie0025@gmail.com>
2015-02-01 19:17:24 -0600

    Remove extra s3 config

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ class User
   include Mongoid::Document
   include Mongoid::Paperclip
   include UserDevise # See this concern file for devise related fields
-  has_mongoid_attached_file :profile_image, bucket: ENV['S3_BUCKET_NAME'],
-    styles: {
-      thumb: '100x100>',
-      square: '200x200#',
-      medium: '300x300>'
-    }
-
+  has_mongoid_attached_file :profile_image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\Z/, allow_blank: :true
