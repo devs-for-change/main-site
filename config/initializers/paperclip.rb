899a536a3d7ea48e9226fb8a81f12e62ab7f4adb
Nic Boie <boie0025@gmail.com>
2015-02-01 19:25:22 -0600

    Re-configure S3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-  Paperclip::Attachment.default_options[:s3_host_name] = 'objects.dreamhost.com'
+  Paperclip::Attachment.default_options[:s3_host_name] = "#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   Paperclip::Attachment.default_options[:s3_protocol] = 'https'
 end
