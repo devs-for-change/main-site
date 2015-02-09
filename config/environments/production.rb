946dd6c30b8af1a5c4b0ea4d91570d4e3d381e71
Nic Boie <boie0025@gmail.com>
2015-02-08 18:02:45 -0600

    Change paperclip env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,13 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => ENV['FOG_DIRECTORY'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
+  }
+  config.fog_credentials = {
+    path_style: true
   }
 
 end
